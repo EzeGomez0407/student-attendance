e246fea2b18c43382740c968c20ca6909507bedc
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2024-08-20 21:44:42 -0300

    fixed index route

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const server = express();
 
+// Routes
+const index = require("./routes/index");
+
+// use Routes
+server.use("/", index);
 const port = process.env.PORT;
 
 server.listen(port, () => {
